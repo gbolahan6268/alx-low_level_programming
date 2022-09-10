2bcc68fce8ad6b8b7294d304ca21c8d4dbe7ff99
gbolahan6268 <olanrewajugbolahan226@gmail.com>
2022-09-09 19:51:07 -0700

    dope

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the sizeof many var types
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizof(d));
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("size of an int: %d byte(s)\n", sizeof(b));
+	printf("size of a long int: %d byte(s)\n", sizeof(c));
+	printf("size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("size of a float: %d byte(s)\n", sizeof(e));
+	return (0);
 }
