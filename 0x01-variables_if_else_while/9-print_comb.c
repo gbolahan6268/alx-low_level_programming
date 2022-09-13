21f2c5516cde99a843937245835dad13b5b68f04
gbolahan6268 <olanrewajugbolahan226@gmail.com>
2022-09-12 18:24:35 -0700

    get

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,13 @@ int main(void)
 {
 	int c;
 
-	for (c = 48; c <= 57; c++)				
+	for (c = 48; c <= 57; c++)
 	{
 		putchar(c);
-		if (c != 57)									
-		{	
-			putchar(',');															putchar(' ');
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
