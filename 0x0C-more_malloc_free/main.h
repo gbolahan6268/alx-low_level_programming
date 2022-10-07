cee6ab44f6cab3e63e5e83c20c8120f2fda6fbf5
gbolahan6268 <olanrewajugbolahan226@gmail.com>
2022-10-07 16:48:35 -0700

    lklkoi

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,31 +1,11 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+#ifndef MAIN_H
+#define MAIN_
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char *c;
-	int *i;
-	float *f;
-	double *d;
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-	c = malloc_checked(sizeof(char) * 1024);
-	printf("%p\n", (void *)c);
-	i = malloc_checked(sizeof(int) * 402);
-	printf("%p\n", (void *)i);
-	f = malloc_checked(sizeof(float) * 100000000);
-	printf("%p\n", (void *)f);
-	d = malloc_checked(INT_MAX);
-	printf("%p\n", (void *)d);
-	free(c);
-	free(i);
-	free(f);
-	free(d);
-	return (0);
-}
+#endif
