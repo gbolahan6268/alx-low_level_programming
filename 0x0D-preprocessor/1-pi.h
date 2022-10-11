8b07c0410a09070b974fd2837ebee3f90b261364
gbolahan6268 <olanrewajugbolahan226@gmail.com>
2022-10-11 08:57:48 -0700

    pluck

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,16 @@
+#ifndef PI_H
+
+#define PI_H
+
+
+
+/*
+ * gbolahan
+ * God is good
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
